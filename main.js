47e9bb179f813711e573e05d492aab570c88cf1f
Mahmoud Ayman Abdalmoaz <https://github.com/mahmoud200015>
2022-12-10 16:29:52 +0200

    first commit

--- main.js
+++ main.js
@@ -3790,6 +3790,115 @@ console.log(url6.match(urlRe));
   Constructor Function
 */
 
+// function User(id, username, salary) {
+//   // object.property = value user input it;
+//   this.i = id;
+//   this.u = username;
+//   this.s = salary + 1000;
+// }
+
+// let userOne = new User(100, 'Elzero', 5000);
+// let userTwo = new User(101, "Hassan", 6000);
+// let userThree = new User(102, "Sayed", 7000);
+
+// console.log(userOne.i);
+// console.log(userOne.u);
+// console.log(userOne.s);
+
+// console.log(userTwo.i);
+// console.log(userTwo.u);
+// console.log(userTwo.s);
+
+// console.log(userThree.i);
+// console.log(userThree.u);
+// console.log(userThree.s);
+
+// const userOne = {
+//   id: 100,
+//   username: "Elzero",
+//   salary: 5000,
+// };
+
+// const userTwo = {
+//   id: 101,
+//   username: "Hassan",
+//   salary: 6000,
+// };
+
+// const userThree = {
+//   id: 102,
+//   username: "Sayed",
+//   salary: 7000,
+// };
+
+/*
+  Constructor Function
+  - New Syntax
+*/
+
+// class User {
+//   constructor(id, username, salary) {
+//     // object.property = value user input it;
+//     this.i = id;
+//     this.u = username;
+//     this.s = salary + 1000;
+//   }
+// }
+
+// let userOne = new User(100, "Elzero", 5000);
+
+// console.log(userOne.i);
+// console.log(userOne.u);
+// console.log(userOne.s);
+
+// console.log(userOne instanceof User);
+// console.log(userOne.constructor === User);
+
+/*
+  Constructor Function
+  - Deal With Properties And Methods
+*/
+
+// class User {
+//   constructor(id, username, salary) {
+//     // Properties
+//     // object.property = value user input it;
+//     this.i = id;
+//     this.u = username || 'Unknown';
+//     this.s = salary < 6000 ? salary + 500 : salary;
+//     this.info = function () {
+//       return `Hello ${this.u}, Your Salary is ${this.s}.`;
+//     };
+//   }
+//   // Methods
+//   writeMsg() {
+//     return `Hello ${this.u}, Your Salary is ${this.s}.`;
+//   }
+// }
+
+// let userOne = new User(100, "Elzero", 5000);
+// let userTwo = new User(101, "", 8000);
+
+// console.log(userOne.i);
+// console.log(userOne.u);
+// console.log(userOne.s);
+// console.log(userOne.info());
+// console.log(userOne.writeMsg());
+// console.log(userTwo.i);
+// console.log(userTwo.u);
+// console.log(userTwo.s);
+// console.log(userTwo.info); // Native Code
+// console.log(userTwo.writeMsg); // Native Code
+
+/*
+  Constructor Function
+  - Update Properties
+  - Built In Constructors
+*/
+
+
+
+
 
 
 
