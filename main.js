84667622ab98c5f68d739bfe147e19785e2c755d
Mahmoud Ayman Abdalmoaz <https://github.com/mahmoud200015>
2022-12-11 23:41:12 +0200

    commit main.js

--- main.js
+++ main.js
@@ -3896,6 +3896,72 @@ console.log(url6.match(urlRe));
   - Built In Constructors
 */
 
+// class User {
+//   constructor(id, username, salary) {
+//     this.i = id;
+//     this.u = username;
+//     this.s = salary;
+//   }
+//   udateName(newName) {
+//     this.u = newName;
+//   }
+// }
+
+// let userOne = new User(100, "Elzero", 5000);
+
+// console.log(userOne.u);
+// userOne.udateName('Mahmoud');
+// console.log(userOne.u);
+
+// let strOne = 'Elzero';
+// let strTwo = new String('Elzero');
+// console.log(typeof strOne);
+// console.log(typeof strTwo);
+// console.log(strOne instanceof String);
+// console.log(strTwo instanceof String);
+// console.log(strOne.constructor === String);
+// console.log(strTwo.constructor === String);
+
+/*
+  Class
+  - Static Properties And Methods
+*/
+
+// class User {
+//   // Static Property
+//   static count = 0;
+//   constructor(id, username, salary) {
+//     this.i = id;
+//     this.u = username;
+//     this.s = salary;
+//     User.count++;
+//   }
+//   // Static Methods
+//   static sayHello() {
+//     return `Hello From class`;
+//   }
+//   static countMembers() {
+//     return `${this.count} count members.`;
+//   }
+// }
+
+
+// let userOne = new User(100, "Elzero", 5000);
+// let userTwo = new User(101, "Ahmed", 5000);
+// let userThree = new User(102, "Sayed", 5000);
+
+// console.log(userOne.u);
+// console.log(userOne.count); // undefined because it's static
+// console.log(User.count); // 0
+// console.log(User.sayHello());
+// console.log(User.countMembers());
+
+/*
+  Class
+  - Inheritance
+*/
+
+
 
 
 
