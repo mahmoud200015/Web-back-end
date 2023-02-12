99f4ddf034f7083263e18f2c2ed8d95bdf49b2f8
Mahmoud Ayman Abdalmoaz <https://github.com/mahmoud200015>
2023-02-12 12:40:45 +0200

    Commit 2/12/2023

--- JavaScript Projects/Elzero Projects/Memory Game/js/scripts.js
+++ JavaScript Projects/Elzero Projects/Memory Game/js/scripts.js
@@ -7,8 +7,12 @@ document.querySelector(".control-buttons span").onclick = function () {
   } else {
     document.querySelector(".info-container .name span").textContent = yourName;
   }
+
+  document.getElementById("start").play();
+
   // Remove control buttons from dom page
   document.querySelector(".control-buttons").remove();
+
 }
 
 // Effect Duration
@@ -30,10 +34,65 @@ console.log(orderRange);
 shuffle(orderRange);
 console.log(orderRange);
 
-// Add order css property to game blocks
 blocks.forEach((block, index) => {
+  // Add order css property to game blocks
   block.style.order = orderRange[index];
+  // Add click event
+  block.addEventListener('click', function () {
+    // Trigger the flipblock function
+    filpBlock(block);
+  });
 });
+
+// Flip Block Function
+function filpBlock(selectedBlock) {
+  // Add class is-flipped
+  selectedBlock.classList.add("is-flipped");
+  // Collect all flip cards
+  let allFlipBlocks = blocks.filter(flipBlock => flipBlock.classList.contains("is-flipped"));
+  // If there are two selected blocks are flipped
+  if (allFlipBlocks.length === 2) {
+    // Stop Clicking funciton
+    stopClicking();
+    // Check matched block function
+    checkMathcedBlocks(allFlipBlocks[0], allFlipBlocks[1]);
+  }
+
+}
+
+// Check matched blocks
+function checkMathcedBlocks(firstBlock, secondBlock) {
+  let tries = document.querySelector(".tries span");
+  
+  if (firstBlock.dataset.technology === secondBlock.dataset.technology) {
+    firstBlock.classList.remove("is-flipped");
+    secondBlock.classList.remove("is-flipped");
+
+    firstBlock.classList.add("has-match");
+    secondBlock.classList.add("has-match");
+
+    document.getElementById("success").play();
+  } else {
+    tries.innerHTML = parseInt(tries.innerHTML) + 1;
+    setTimeout(() => { 
+      firstBlock.classList.remove("is-flipped");
+      secondBlock.classList.remove("is-flipped");
+    }, duration)
+
+    document.getElementById("wrong").play();
+  }
+}
+
+// Stop Clickign funciton
+function stopClicking() {
+  // Another way (Add class no-click on main container and put css property in main file css)
+  blocksContainer.style.pointerEvents = "none";
+  setTimeout(() => {
+    // Remove css property
+    blocksContainer.style.removeProperty("pointer-events");
+    // blocksContainer.style.pointerEvents = "";
+  }, duration);
+}
 
 // Shuffle function
 function shuffle(array) {
@@ -60,11 +119,3 @@ function shuffle(array) {
   return array;
 }
 
-
-
-let block = document.querySelectorAll(".game-blocks .game-block");
-block.forEach((box) => {
-  box.addEventListener('click', (e) => {
-    box.classList.toggle('is-flipped');
-  })
-});
